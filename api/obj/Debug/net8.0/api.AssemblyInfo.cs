0f71700476a08ca0e50ef9b52471cf18619ee166
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-06 20:32:46 -0700

    added how to create API for sqlite

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0654cf9d81cb531a49a03993ce8db6a94b93e312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a29bd8f39e082206181b5c61217a7c06077fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
