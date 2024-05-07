a56c0c68c4f7026a440683a4cffbe9f8e1d21bd7
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-06 21:32:20 -0700

    checked to see if code still works before submission.  data in todo database simply edited with swagger api interface.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a29bd8f39e082206181b5c61217a7c06077fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efd3e54293dadb7ef76c14ab677a3c7ca8e4ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
